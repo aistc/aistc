2be75b8817c49ff85e252779c79f317289fbb463
aistc <82798806+aistc@users.noreply.github.com>
2021-04-20 09:04:10 +0800

    updata

--- play.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sourl = $_GET['url'];
-$playurl = "https://parse.xymov.net?url=".$sourl;
-echo("<html><body style='margin:0; padding:0'>");
-echo('<iframe src ="'.$playurl.'" style="border:0;" width="100%" height="100%"></iframe>');
-echo("</body></html>");
-?>
